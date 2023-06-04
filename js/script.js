bbcc351340c3bd9348d5d386009c298ec86b1950
AmanyHassanseleem <130002416+AmanyHassanseleem@users.noreply.github.com>
2023-06-04 10:35:00 +0200

    project3

---
+++ js/script.js
@@ -0,0 +1,130 @@
+// document.write('hello');
+// document.getElementById('div1').innerHTML="yyyyyyyyyyyyy";
+// document.getElementById('btn1').onclick =function(){
+//     document.getElementById('div1').innerHTML ='gggfg';
+// }
+
+
+// console.log('tttttr');
+// console.log(200+700);
+// alert ('take care')
+
+// prompt ('enter your name?')
+
+// var num1 = 20
+// var num2 = 60
+// console.log(num1+num2)
+// /////////////////////////////////////////////////
+//data types:number string array null object boolean undefined  7
+
+// var x=70
+// x=90
+// x=3
+// console.log(x)
+// console.log(typeof x)
+//////////////////////////////////
+
+/*
+ممنوع تسمي متغير يبتدي برقم
+الاسم يدل علي معني
+ممنوع استخدام الكلمات المحجوزة
+boolean مش رقم ولا سترنج true or false
+string اي حاجة بين كوتيشن
+
+*/
+////////////////////////
+// var x = 'amany'
+// console.log(x)
+// console.log(typeof x)
+
+///////////////////////////////
+// var x = true
+// console.log(typeof x)
+
+///////////////////////////
+
+// var x = [ 'hmghg' ,'fvhvghjv' ,19 , 'sesersre' , true]
+// console.log(x)
+// console.log(typeof x)
+// console.log(x[1])
+// console.log(typeof x[1])
+///////////////////////////////
+// var person = {
+//     name : 'bjbjb' ,
+//     age : '27' ,
+//     city : 'gjhgjh'
+
+// }
+// console.log(person)
+// console.log(typeof person)
+
+// console.log(person.age)
+// console.log(person.city)
+///////////////////////////////////
+
+//   var x 
+//  console.log(x)
+//  console.log(typeof x)
+
+
+// var y=2;
+// console.log(x%y);
+// x++
+// ++x
+// console.log(x)
+
+// var x= prompt('enter your degree')
+// if (x>=80){
+//   console.log('excellent')
+// } else if (x>75){
+//   console.log('very good')
+// }else if (x>60){
+//   console.log('good')
+// }else{
+//   console.log('fair')
+// }
+
+// var x= 6
+// var y= 8
+// var z= 10
+// if (!(!(x== 6) && !(y ==20))|| ((x==6 ||y==10) && (z==10 ||y==8) )){
+//   console.log('yes')
+// }else{
+//   console.log('no')
+// }
+
+/*hujhj
+uhbuj
+
+hkjhk*/
+
+// document.write('<h1>elzero</h1>');
+// document.querySelector('h1').style.color='blue';
+// document.querySelector('h1').style.fontSize='80px';
+// document.querySelector('h1').style.fontFamily='bold';
+// document.querySelector('h1').style.textAlign='center';
+// document.querySelector('h1').style.fontFamily='arial';
+
+// console.log('%celzero %cweb %cschool', 'color:red;font-size:40px','color:green;font-size:40px;font-weight:bold','color:white;font-size:40px;background-color:blue');
+
+// console.group('group 1')
+// console.log('message one');
+// console.log('message two');
+// console.group('child group');
+// console.log('message one');
+// console.log('message two');
+// console.group('grand child group')
+// console.log('message one');
+// console.log('message two');
+// console.groupEnd()
+// console.groupEnd()
+// console.groupEnd()
+// console.group('group 2')
+// console.log('message one');
+// console.log('message two');
+
+// console.table(['zero' ,'ahmed','mohamed','mona','rana']);
+
+/*console.log("Iam In Console");
+document.write("Iam In Page");*/
+
